f5298260541cf5a84702b6cb738e759d25d0718b
SEUNGHYUN-PARK <shstyle812@gmail.com>
2018-04-28 11:57:27 +0900

    Accepted without Stack

---
+++ Leetcode/657. Judge Route Circle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x=0,y=0;
+        for(int i=0;i<moves.size();i++)
+        {
+            if(moves[i]=='U')
+                x++;
+            if(moves[i]=='D')
+                x--;
+            if(moves[i]=='L')
+                y++;
+            if(moves[i]=='R')
+                y--;   
+        }
+        if(x==0&&y==0) return true;
+        else return false;
+    }
+};
