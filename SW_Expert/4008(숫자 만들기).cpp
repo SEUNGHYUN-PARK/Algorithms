a772bbd2edd3d0fcfcd4d74782cb21d28f18434c
PARKSEUNGHYUN <shstyle812@gmail.com>
2018-09-17 03:37:37 +0900

    Recursive

--- SW_Expert/4008(숫자 만들기).cpp
+++ SW_Expert/4008(숫자 만들기).cpp
@@ -1,51 +1,47 @@
 #include <iostream>
 #include <cstdio>
-#include <memory.h>
+#include <algorithm>
 using namespace std;
-int max_num;
-int min_num;
-int result;
-int arr[12]={0,};
-int n;
-int op[4]={0,}; // + - * /
-void go(int idx, int p, int s, int m, int d, int current)
+int maxnum = -2100000000;
+int minnum = 2100000000;
+
+void go(int num[12], int index, int p, int s, int m, int d, int current, int n)
 {
-    if(idx == n-1)
-    {
-        max_num = max(current,max_num);
-        min_num = min(current,min_num);
-        result = max_num - min_num;
-        return;
-    }
-    if(p<op[0])
-        go(idx+1,p+1,s,m,d,current+arr[idx+1]);
-    if(s<op[1])
-        go(idx+1,p,s+1,m,d,current-arr[idx+1]);
-    if(m<op[2])
-        go(idx+1,p,s,m+1,d,current*arr[idx+1]);
-    if(d<op[3])
-        go(idx+1,p,s,m,d+1,current/arr[idx+1]);
+	if (index >= n-1)
+	{
+		minnum = min(minnum, current);
+		maxnum = max(maxnum, current);
+		return;
+	}
+	if (p > 0)
+		go(num, index + 1, p - 1, s, m, d, current + num[index+1], n);
+	if (s > 0)
+		go(num, index + 1, p, s - 1, m, d, current - num[index+1], n);
+	if (m > 0)
+		go(num, index + 1, p, s, m - 1, d, current * num[index+1], n);
+	if (d > 0)
+		go(num, index + 1, p, s, m, d - 1, current / num[index+1], n);
 }
+
 int main(void)
 {
-    int T;
-    scanf("%d",&T);
-    for(int test_case = 1; test_case<=T; test_case++)
-    {
-        memset(arr, 0, 12);
-        memset(op, 0, 4);
-        max_num = -100000000;
-        min_num = 100000000;
-        scanf("%d",&n);
-        for(int i=0;i<4;i++)
-        {
-            scanf("%d",&op[i]);
-        }
-        for(int i=0;i<n;i++)
-        {
-            scanf("%d",&arr[i]);
-        }
-        go(0,0,0,0,0,arr[0]);
-        printf("#%d %d\n",test_case,result);
-    }
+	int T;
+	scanf("%d", &T);
+	for (int testcase = 1; testcase <= T; testcase++)
+	{
+		int numarr[12];
+		int n;
+		int p, s, m, d;
+		maxnum = -2100000000;
+		minnum = 2100000000;
+		scanf("%d", &n);
+		scanf("%d%d%d%d", &p, &s, &m, &d);
+		for (int i = 0; i < n; i++)
+		{
+			scanf("%d", &numarr[i]);
+		}
+		go(numarr, 0, p, s, m, d, numarr[0],n);
+
+		printf("#%d %d\n", testcase, maxnum - minnum);
+	}
 }
